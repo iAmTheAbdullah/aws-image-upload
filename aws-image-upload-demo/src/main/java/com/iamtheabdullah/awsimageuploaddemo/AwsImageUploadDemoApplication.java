e3792cf6750d05ac5544f337c7f1ee583963fe41
Abdullah Malik <asmalik250@gmail.com>
2020-04-11 17:10:12 +0100

    Initialise spring boot project

---
+++ aws-image-upload-demo/src/main/java/com/iamtheabdullah/awsimageuploaddemo/AwsImageUploadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.iamtheabdullah.awsimageuploaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageUploadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageUploadDemoApplication.class, args);
+	}
+
+}
---
+++ aws-image-upload-demo/src/test/java/com/iamtheabdullah/awsimageuploaddemo/AwsImageUploadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamtheabdullah.awsimageuploaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
